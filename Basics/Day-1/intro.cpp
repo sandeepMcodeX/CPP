1f05f65d57c34c36bdcab636a24f99e2169c1dc4
sandeep <sandeepmcodex.dev@gmail.com>
2025-06-29 20:07:29 +0530

    Update intro.cpp

--- Basics/Day-1/intro.cpp
+++ Basics/Day-1/intro.cpp
@@ -5,3 +5,11 @@ Intro to Programming
  Base2  , Base8 , Base16 
  Machine Lang , Assembly Lang , HighLevel Lang 
 */
+
+#include <iostream>         // Includes input/output library
+using namespace std;        // Allows use of 'cout' without std:: prefix
+
+int main() {                // Entry point of the program
+    cout << "Hello world";  // Prints "Hello world" to the console
+    return 0;               // Ends the program
+}
