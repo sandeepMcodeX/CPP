77933151732dc96be99f0e8edfcce49b040b00de
sandeep <sandeepmcodex.dev@gmail.com>
2025-07-02 22:47:40 +0530

    Add files via upload

---
+++ Basics/Day-4/01_program.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    if(num>0){
+        cout<<"The given number is Positive Number"<<num;
+
+    }
+    else{
+        cout<<" The given number is Negative Number"<<num;
+    }
+}
