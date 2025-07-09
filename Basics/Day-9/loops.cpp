73efd6632e99fabc2bcbf659cf53ddedc7f4df36
sandeep <sandeepmcodex.dev@gmail.com>
2025-07-09 23:34:46 +0530

    Add files via upload

---
+++ Basics/Day-9/loops.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int n = 10, i = 1;
+    // while (i <= n) {
+    //     cout << i << " ";
+    //     i++;
+    // }
+
+
+    //  int n = 1234, sum = 0;
+    // while (n > 0) {
+    //     sum += n % 10;
+    //     n /= 10;
+    // }
+    // cout << "Sum of digits: " << sum;
+
+
+
+    // int n = 5, fact = 1;
+    // while (n > 0) {
+    //     fact *= n;
+    //     n--;
+    // }
+    // cout << "Factorial: " << fact;
+
+
+
+    // int n = 12345, rev = 0;
+    // while (n > 0) {
+    //     rev = rev * 10 + (n % 10);
+    //     n /= 10;
+    // }
+    // cout << "Reversed: " << rev;
+    return 0;
+}
